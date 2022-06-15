30cb393f09d1f2dd58048f2789c1db85e2c43551
Leonardo Bein <leonardobein@gmail.com>
2022-06-15 15:42:09 -0300

    chore: lint in code

--- src/Utils/cache.ts
+++ src/Utils/cache.ts
@@ -13,26 +13,26 @@ type WaCacheMap<T=any> = {[key: string]: WaCacheData<T>};
 
 export default class WaCache<T = any> {
 
-    private _cacheData: WaCacheMap<T> = {};
-    private _timer: NodeJS.Timeout;
-    private _timeoutMs: number;
-    private _logger?: Logger;
+	private _cacheData: WaCacheMap<T> = {}
+	private _timer: NodeJS.Timeout
+	private _timeoutMs: number
+	private _logger?: Logger
 
-    constructor(timeoutMs: number, config?: SocketConfig) {
+	constructor(timeoutMs: number, config?: SocketConfig) {
     	this._timeoutMs = timeoutMs
     	this._timer = setInterval(this._cleanCache.bind(this), 2_000)
     	this._logger = config?.logger
-    }
+	}
 
-    private _cacheIsExpired(c: WaCacheData<T>): boolean {
+	private _cacheIsExpired(c: WaCacheData<T>): boolean {
     	const now = new Date()
     	return now > c.timeout
-    }
+	}
 
-    private _cleanCache() {
+	private _cleanCache() {
     	const removeKeys = []
 
-        for(const [key, cache] of Object.entries(this._cacheData)) {
+    	for(const [key, cache] of Object.entries(this._cacheData)) {
     		if(this._cacheIsExpired(cache)) {
     			this._logger?.debug({ cache }, 'cache expired, removing')
     			removeKeys.push(key)
@@ -43,10 +43,10 @@ export default class WaCache<T = any> {
     		const key = removeKeys[index]
     		delete this._cacheData[key]
     	}
-    }
+	}
 
 
-    getCache(key: string): T | undefined {
+	getCache(key: string): T | undefined {
     	const cache = this._cacheData[key]
 
     	if(!cache || this._cacheIsExpired(cache)) {
@@ -54,26 +54,26 @@ export default class WaCache<T = any> {
     	}
 
     	return cache.data
-    }
+	}
 
-    putCache(key: string, data: T) {
+	putCache(key: string, data: T) {
     	const now = new Date()
     	const timeout = new Date(now.getTime() + this._timeoutMs)
 
     	this._cacheData[key] = { data, timeout }
-    }
+	}
 
-    removeCache(key: string) {
+	removeCache(key: string) {
     	const cache = this.getCache(key)
 
     	if(cache) {
     		this._logger?.debug({ cache }, 'removing cache.')
     		delete this._cacheData[key]
     	}
-    }
+	}
 
-    stop() {
+	stop() {
     	clearInterval(this._timer)
-    }
+	}
 
 }

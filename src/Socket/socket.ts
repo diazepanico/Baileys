45927fea34c51a1e4aac188a7da5311004162ca5
Leonardo Bein <leonardobein@gmail.com>
2022-07-05 12:03:37 -0300

    refactor: more logs in sendMessage

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -69,6 +69,7 @@ export const makeSocket = ({
 			throw new Boom('Connection Closed', { statusCode: DisconnectReason.connectionClosed })
 		}
 
+		logger.debug({data_length: data.length}, 'encode frame')
 		const bytes = noise.encodeFrame(data)
         await sendPromise.call(ws, bytes) as Promise<void>
 	}
@@ -80,6 +81,7 @@ export const makeSocket = ({
 		}
 
 		const buff = encodeBinaryNode(frame)
+		logger.debug({buff_length: buff.length}, 'encode binary node in bytes')
 		return sendRawMessage(buff)
 	}
 

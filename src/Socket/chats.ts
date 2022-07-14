65426c6813acd3e9b06db97d6d19765901695319
Leonardo Bein <leonardobein@gmail.com>
2022-07-14 09:42:26 -0300

    feat: change profile name

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -192,6 +192,10 @@ export const makeChatsSocket = (config: SocketConfig) => {
 		})
 	}
 
+	const updateProfileName = async(name: string) => {
+		return await chatModify({ pushNameSetting: name }, '')
+	}
+
 	const fetchBlocklist = async() => {
 		const result = await query({
 			tag: 'iq',
@@ -786,6 +790,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 		fetchStatus,
 		updateProfilePicture,
 		updateProfileStatus,
+		updateProfileName,
 		updateBlockStatus,
 		getBusinessProfile,
 		resyncAppState,
